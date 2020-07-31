837de23de1284f8b734565778120813f93a2a526
Aditya Narayanaswamy <anarayan@redhat.com>
2020-07-31 00:49:38 -0400

    Metrics: Add Metrics API for installer telemetry data gathering
    Adds the main functions that the installer will call to create metrics,
    store information and push to any endpoint required. The system has
    an opt out policy where if the user sets the OPENSHIFT_INSTALL_DISABLE_METRICS
    environment flag, the metrics will not be sent.
    
    The user can also change the endpoint by setting the environment
    variable OPENSHIFT_INSTALL_METRICS_ENDPOINT to a required URL
    endpoint and the system will redirect the metrics.
    
    The system creates the metrics when the values are being sent and pushes them
    only if the value/labels are set to them.
    
    The configuration right now is to switch the system on only when the URL is
    set.

---
+++ pkg/metrics/gatherer/utility.go
@@ -0,0 +1,74 @@
+package gatherer
+
+import (
+	"runtime"
+
+	"github.com/openshift/installer/pkg/metrics/timer"
+	"github.com/openshift/installer/pkg/version"
+)
+
+// InitializeInvocationMetrics is a utility function that adds the common information to create
+// an invocation metrics.
+func InitializeInvocationMetrics(metricName string) {
+	AddLabelValue(metricName, "result", "Success")
+	AddLabelValue(metricName, "returnCode", "1")
+	AddLabelValue(metricName, "os", runtime.GOOS)
+	CurrentInvocationContext = metricName
+}
+
+// LogError sets the result and returnCode to the right values and pushes the information to the
+// gateway. Must be called before the installer breaks execution.
+func LogError(err string, metricName string) {
+	AddLabelValue(metricName, "result", err)
+	AddLabelValue(metricName, "returnCode", "0")
+
+	SendPrometheusInvocationData(metricName)
+}
+
+// SendPrometheusInvocationData gets the timer information for the duration metric and pushes it to
+// the gateway.
+func SendPrometheusInvocationData(metricName string) {
+	duration := timer.StopTimer(timer.TotalTimeElapsed)
+	SetValue(metricName, duration.Minutes())
+	version, err := version.Version()
+	if err != nil {
+		AddLabelValue(CurrentInvocationContext, "version", version)
+	}
+	PushAll()
+}
+
+// UpdateDurationMetricsWithError sets all the duration metrics to error message.
+func UpdateDurationMetricsWithError(err string) {
+	listOfDurationMetrics := []string{
+		DurationAPIMetricName,
+		DurationBootstrapMetricName,
+		DurationInfrastructureMetricName,
+		DurationOperatorsMetricName,
+		DurationProvisioningMetricName,
+	}
+
+	for _, item := range listOfDurationMetrics {
+		AddLabelValue(item, "result", err)
+	}
+}
+
+// UpdateDurationMetrics initializes the duration metrics to success.
+func UpdateDurationMetrics(platform string) {
+	listOfDurationMetrics := []string{
+		DurationAPIMetricName,
+		DurationBootstrapMetricName,
+		DurationInfrastructureMetricName,
+		DurationOperatorsMetricName,
+		DurationProvisioningMetricName,
+	}
+
+	for _, item := range listOfDurationMetrics {
+		AddLabelValue(item, "platform", platform)
+		AddLabelValue(item, "result", "success")
+		version, err := version.Version()
+		if err == nil {
+			AddLabelValue(item, "version", version)
+		}
+
+	}
+}
