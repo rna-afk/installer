84441fa6b96b63923c8b1b3c1d7982b611bbd5c7
rna-afk <anarayan@redhat.com>
2022-11-28 12:59:43 -0500

    azure: Create ControlPlaneMachineSet CRDs
    Creating an extra CRD asset for the ControlPlaneMachineSet which
    is required for the machine api operator for more control over
    the control-plane nodes that come up on Azure.

--- pkg/asset/machines/master.go
+++ pkg/asset/machines/master.go
@@ -368,8 +368,7 @@ func (m *Master) Generate(dependencies asset.Parents) error {
 			return err
 		}
 		useImageGallery := installConfig.Azure.CloudName != azuretypes.StackCloud
-
-		machines, err = azure.Machines(clusterID.InfraID, ic, &pool, string(*rhcosImage), "master", masterUserDataSecretName, capabilities, useImageGallery)
+		machines, controlPlaneMachineSet, err = azure.Machines(clusterID.InfraID, ic, &pool, string(*rhcosImage), "master", masterUserDataSecretName, capabilities, useImageGallery)
 		if err != nil {
 			return errors.Wrap(err, "failed to create master machine objects")
 		}
