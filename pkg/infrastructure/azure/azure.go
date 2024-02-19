eecd95a616b427bfce46da34ef5c8b8e19ebc08f
rna-afk <anarayan@redhat.com>
2024-02-19 14:08:49 -0500

    azure: Create DNS entries for azure CAPI
    Creating the DNS entries for CAPI implementation of Azure.

--- pkg/infrastructure/azure/azure.go
+++ pkg/infrastructure/azure/azure.go
@@ -1,6 +1,8 @@
 package azure
 
 import (
+	"context"
+
 	"github.com/openshift/installer/pkg/infrastructure/clusterapi"
 	azuretypes "github.com/openshift/installer/pkg/types/azure"
 )
@@ -12,3 +14,7 @@ type Provider struct{}
 
 // Name gives the name of the provider, Azure.
 func (*Provider) Name() string { return azuretypes.Name }
+
+func (p *Provider) InfraReady(ctx context.Context, in clusterapi.InfraReadyInput) error {
+	return createAzureDNSEntries(ctx, in)
+}
