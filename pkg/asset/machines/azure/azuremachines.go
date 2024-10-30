67f781e898311073f9fa2576bcc16b3e7922cf8e
rna-afk <anarayan@redhat.com>
2024-10-30 10:54:41 -0400

    azure: Enable option to set boot diagnostics
    Enabling the option for the users to set the type of boot
    diagnostics for the bootstrap and control plane machines
    for log collection.

--- pkg/asset/machines/azure/azuremachines.go
+++ pkg/asset/machines/azure/azuremachines.go
@@ -25,15 +25,16 @@ const (
 
 // MachineInput defines the inputs needed to generate a machine asset.
 type MachineInput struct {
-	Subnet          string
-	Role            string
-	UserDataSecret  string
-	HyperVGen       string
-	UseImageGallery bool
-	Private         bool
-	UserTags        map[string]string
-	Platform        *azure.Platform
-	Pool            *types.MachinePool
+	Subnet            string
+	Role              string
+	UserDataSecret    string
+	HyperVGen         string
+	UseImageGallery   bool
+	Private           bool
+	UserTags          map[string]string
+	Platform          *azure.Platform
+	Pool              *types.MachinePool
+	StorageAccountURI string
 }
 
 // GenerateMachines returns manifests and runtime objects to provision the control plane (including bootstrap, if applicable) nodes using CAPI.
@@ -144,6 +145,31 @@ func GenerateMachines(clusterID, resourceGroup, subscriptionID string, in *Machi
 		}
 	}
 
+	var defaultDiag *capz.Diagnostics
+	if in.Platform.DefaultMachinePlatform != nil && in.Platform.DefaultMachinePlatform.BootDiagnostics != nil {
+		defaultDiag = &capz.Diagnostics{
+			Boot: &capz.BootDiagnostics{
+				StorageAccountType: in.Platform.DefaultMachinePlatform.BootDiagnostics.Type,
+			},
+		}
+		defaultURI := in.StorageAccountURI
+		if saURI := in.Platform.DefaultMachinePlatform.BootDiagnostics.StorageAccountURI; saURI != "" {
+			defaultURI = saURI
+		}
+
+		defaultDiag.Boot.UserManaged = &capz.UserManagedBootDiagnostics{
+			StorageAccountURI: defaultURI,
+		}
+	}
+
+	controlPlaneDiag := defaultDiag
+	if mpool.BootDiagnostics != nil {
+		controlPlaneDiag.Boot.StorageAccountType = mpool.BootDiagnostics.Type
+		if mpool.BootDiagnostics.StorageAccountURI != "" {
+			controlPlaneDiag.Boot.UserManaged.StorageAccountURI = mpool.BootDiagnostics.StorageAccountURI
+		}
+	}
+
 	var result []*asset.RuntimeFile
 	for idx := int64(0); idx < total; idx++ {
 		zone := mpool.Zones[int(idx)%len(mpool.Zones)]
@@ -172,7 +198,8 @@ func GenerateMachines(clusterID, resourceGroup, subscriptionID string, in *Machi
 						AcceleratedNetworking: ptr.To(mpool.VMNetworkingType == string(azure.VMnetworkingTypeAccelerated) || mpool.VMNetworkingType == string(azure.AcceleratedNetworkingEnabled)),
 					},
 				},
-				Identity: capz.VMIdentityUserAssigned,
+				Identity:    capz.VMIdentityUserAssigned,
+				Diagnostics: controlPlaneDiag,
 				UserAssignedIdentities: []capz.UserAssignedIdentity{
 					{
 						ProviderID: userAssignedIdentityID,
@@ -233,6 +260,7 @@ func GenerateMachines(clusterID, resourceGroup, subscriptionID string, in *Machi
 			AdditionalCapabilities:     additionalCapabilities,
 			SecurityProfile:            securityProfile,
 			Identity:                   capz.VMIdentityUserAssigned,
+			Diagnostics:                controlPlaneDiag,
 			UserAssignedIdentities: []capz.UserAssignedIdentity{
 				{
 					ProviderID: userAssignedIdentityID,
